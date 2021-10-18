8f50436a02a4f806237d949204a85b8f521016a5
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-10-18 15:55:14 +0200

    2

--- app/src/main/java/com/example/imagebutton/MainActivity.kt
+++ app/src/main/java/com/example/imagebutton/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.imagebutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageButton = findViewById<ImageButton>(R.id.imageButton)
+        imageButton?.setOnClickListener { Toast.makeText(this@MainActivity, R.string.image_button_clicked, Toast.LENGTH_SHORT).show()
     }
+}
+
 }
